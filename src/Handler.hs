205dc884ca93bfce6646051a42f72604cb7335cf
Tim McEwan <tim@mcewan.it>
2019-08-24 23:32:22 +1000

    Handle API Gateway events

---
+++ src/Handler.hs
@@ -0,0 +1,156 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric, DeriveAnyClass #-}
+
+module Handler
+  ( handler
+  ) where
+
+import AWSLambda.Events.APIGateway (APIGatewayProxyRequest, APIGatewayProxyResponse(..))
+import Control.Exception (Exception, throw)
+import Control.Lens ((<&>), (^.), set)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Trans.AWS
+  ( Credentials(Discover)
+  , LogLevel(Debug)
+  , Region(NorthVirginia)
+  , envLogger
+  , newEnv
+  , newLogger
+  , runAWST
+  , runResourceT
+  , send
+  , within
+  )
+import Data.Aeson (FromJSON, ToJSON, Value, (.=), decode, defaultOptions, encode, genericToEncoding, object, toEncoding)
+import qualified Data.ByteString.Internal as BSI
+import Data.ByteString.Lazy (ByteString, fromStrict)
+import Data.Maybe (fromMaybe)
+import Data.Text (Text, pack)
+import Data.Typeable (Typeable)
+import GHC.Generics (Generic)
+import Network.AWS.Data (toBS)
+import Network.AWS.Lambda.Invoke (invoke, irsPayload)
+import Network.HTTP.Req
+  ( POST(..)
+  , ReqBodyJson(..)
+  , (/:)
+  , defaultHttpConfig
+  , https
+  , jsonResponse
+  , req
+  , responseBody
+  , runReq
+  )
+import System.Environment (lookupEnv)
+import System.IO (stdout)
+
+data Details =
+  Details
+    { firstName :: Text
+    , lastName :: Text
+    , email :: Text
+    , address :: Text
+    , suburb :: Text
+    , postcode :: Text
+    , dob :: Text
+    , phone :: Text
+    , crn :: Text
+    , debtReason :: Text
+    , personalCircumstances :: [Text]
+    }
+  deriving (Show, Generic)
+
+data CustomException =
+  BadLetter
+  deriving (Show, Typeable)
+
+instance Exception CustomException
+
+instance ToJSON Details where
+  toEncoding = genericToEncoding defaultOptions
+
+event :: ByteString
+event =
+  encode
+    (Details
+       { firstName = pack "alice"
+       , lastName = pack "citizen"
+       , email = pack "tim+alicecitizen@mcewan.it"
+       , address = pack "7 henry dr"
+       , suburb = pack "picnic point"
+       , postcode = pack "2341"
+       , dob = pack "01/01/1900"
+       , phone = pack "0123456789"
+       , crn = pack "123456789x"
+       , debtReason = pack "Lorem ipsum dolor sit amet."
+       , personalCircumstances = [pack "Addiction", pack "sfdg sdfgsdfg dsfg"]
+       })
+
+handler :: APIGatewayProxyRequest Text -> IO (APIGatewayProxyResponse Text)
+handler request = do
+  print request
+  authEmail <- fromEnv "DOCSAWAY_EMAIL" ""
+  key <- fromEnv "DOCSAWAY_KEY" ""
+  apiMode <- fromEnv "API_MODE" "TEST"
+  rsp <- invokeLambda NorthVirginia (pack "fraudstop-dev-letter-func") event
+  let letter = decode (fromStrict rsp) :: Maybe LambdaResponse
+  case letter of
+    Just pdf -> do
+      result <- sendLetter authEmail key apiMode (body pdf)
+      print result
+    _ -> throw BadLetter
+  pure responseOk
+
+data LambdaResponse =
+  LambdaResponse
+    { body :: String
+    }
+  deriving (Show, Generic, FromJSON)
+
+invokeLambda :: Region -> Text -> ByteString -> IO BSI.ByteString
+invokeLambda region funcName payload = do
+  lgr <- newLogger Debug stdout
+  env <- newEnv Discover <&> set envLogger lgr
+  runResourceT . runAWST env . within region $ do
+    rsp <- send $ invoke funcName $ toBS payload
+    let Just output = rsp ^. irsPayload
+    return output
+
+sendLetter :: (MonadIO m, ToJSON v1, ToJSON v2, ToJSON v3, ToJSON v4) => v1 -> v2 -> v3 -> v4 -> m Value
+sendLetter authEmail key apiMode letter = do
+  runReq defaultHttpConfig $ do
+    let endpoint = https "www.docsaway.com" /: "app" /: "api" /: "rest" /: "mail.json"
+    let apiConnection = object ["email" .= authEmail, "key" .= key]
+    let printingStation =
+          object ["id" .= ("AUTO" :: Text), "courierID" .= False, "ink" .= ("BW" :: Text), "paper" .= ("80" :: Text)]
+    let recipient =
+          object
+            [ "name" .= ("Centrelink" :: Text)
+            , "company" .= ("" :: Text)
+            , "address1" .= ("Reply Paid 7800" :: Text)
+            , "address2" .= ("" :: Text)
+            , "city" .= ("Canberra BC" :: Text)
+            , "state" .= ("ACT" :: Text)
+            , "zip" .= ("2610" :: Text)
+            , "country" .= ("AU" :: Text)
+            ]
+    let payload =
+          object
+            [ "APIConnection" .= apiConnection
+            , "APIMode" .= apiMode
+            , "APIReport" .= True
+            , "PDFFile" .= letter
+            , "PrintingStation" .= printingStation
+            , "Recipient" .= recipient
+            ]
+    r <- req POST endpoint (ReqBodyJson payload) jsonResponse mempty
+    return (responseBody r :: Value)
+
+fromEnv :: String -> String -> IO String
+fromEnv var fallback = do
+  envVar <- lookupEnv var
+  return $ fromMaybe fallback envVar
+
+responseOk :: APIGatewayProxyResponse body
+responseOk = APIGatewayProxyResponse 200 [] Nothing
+-- response404 :: APIGatewayProxyResponse Text
+-- response404 = APIGatewayProxyResponse 404 [] Nothing
