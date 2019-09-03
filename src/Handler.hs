337340702239d174e0eb8ccf04bd8852d1d7841b
Tim McEwan <tim@mcewan.it>
2019-09-03 23:26:10 +1000

    ok...

--- src/Handler.hs
+++ src/Handler.hs
@@ -6,13 +6,11 @@ import AWSLambda.Events.APIGateway
   , requestBody
   )
 import Control.Lens ((^.))
-import Data.Aeson (FromJSON, ToJSON, decode, defaultOptions, genericToEncoding, toEncoding)
 
-import Data.ByteString.Lazy (ByteString, fromStrict)
+import Data.Aeson (FromJSON, ToJSON, decode, defaultOptions, genericToEncoding, toEncoding)
+import Data.ByteString.Lazy (fromStrict)
 import Data.Text (Text)
 import Data.Text.Encoding (encodeUtf8)
-
--- import Data.Typeable (typeOf)
 import GHC.Generics (Generic)
 
 data Details =
@@ -36,18 +34,14 @@ instance ToJSON Details where
 
 instance FromJSON Details
 
-recode :: Text -> ByteString
-recode = fromStrict . encodeUtf8
+detailDecode :: Text -> Maybe Details
+detailDecode = decode . fromStrict . encodeUtf8
 
 handler :: APIGatewayProxyRequest Text -> IO (APIGatewayProxyResponse Text)
 handler request = do
   let body = request ^. requestBody
-  case body of
-    Just bod -> do
-      let deets = decode (recode bod) :: Maybe Details
-      print $ deets
-      -- case (decode bod :: Maybe Details) of
-      --   Just deets -> pure $ response 200
-      --   Nothing -> pure $ response 403
+  case detailDecode =<< body of
+    Just details -> do
+      print details
       pure $ APIGatewayProxyResponse 404 [] Nothing
     _ -> pure $ APIGatewayProxyResponse 404 [] Nothing
